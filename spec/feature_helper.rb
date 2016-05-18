536af9919cc60fb66fa8608bacde34a72715d9ea
Alex Brinkman <abrink@commercekitchen.com>
2016-05-18 10:51:02 -0600

    [TOOL] Setup testing: RSpec/Capybara/Factory Girl

---
+++ spec/feature_helper.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+require "capybara/rails"
+require "capybara/rspec"
+
+
+Capybara.javascript_driver = :webkit
+# Capybara.javascript_driver = :selenium
+Capybara::Webkit.configure do |config|
+  # config.debug = true # Uncomment for error information.
+end
+
+Capybara.configure do |config|
+  config.always_include_port = true
+end
