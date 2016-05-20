e42370e2cbde55144a1de9d382b798f083db6539
Alex Brinkman <abrink@commercekitchen.com>
2016-05-20 12:41:27 -0600

    [FEAT] Add sign up form to homepage

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -2,8 +2,18 @@ require "feature_helper"
 
 feature "A user signs up" do
 
-  scenario "with valid credentials" do
+  scenario "from default sign up path, with valid credentials" do
     visit new_user_registration_path
+    find("#user_email").set("user@showami.com")
+    find("#user_password").set("asdfasdf")
+    find("#user_password_confirmation").set("asdfasdf")
+    click_button "Sign up"
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.")
+  end
+
+  scenario "from root path, with valid credentials" do
+    visit root_path
     find("#user_email").set("user@showami.com")
     find("#user_password").set("asdfasdf")
     find("#user_password_confirmation").set("asdfasdf")
