c2d4e39d95353fa76350869df69f006b59873666
Alex Brinkman <abrink@commercekitchen.com>
2016-05-19 22:45:10 -0600

    [CHG] Add Sendgrid as smtp provider

---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,19 @@
+# Initialize ActionMailer settings for sendgrid
+
+login = Rails.application.secrets.sendgrid_login
+password = Rails.application.secrets.sendgrid_password
+domain = Rails.application.secrets.sendgrid_domain || "showami.com"
+
+if login.nil? && password.nil?
+  abort("Please ensure the sendgrid_login and sendgrid_password are defined in secrets.yml")
+else
+  ActionMailer::Base.smtp_settings = {
+    user_name: login,
+    password: password,
+    domain: domain,
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+end
