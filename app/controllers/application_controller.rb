f8256265d9679b3be7530e7c17efecdc4321b757
Alex Brinkman <abrink@commercekitchen.com>
2016-06-15 09:33:05 -0600

    [REFAC] Fix rubocop violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_greeting
-
-  before_filter :set_paper_trail_whodunnit # To make current_user available to model auditing
+  before_action :set_paper_trail_whodunnit # To make current_user available to model auditing
 
   def after_sign_in_path_for(resource)
     stored_location = stored_location_for(resource) # This resets after being called, so store.
