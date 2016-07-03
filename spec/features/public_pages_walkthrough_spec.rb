d48c016422d8f71a34d09e302f3a835251fd9704
Alex Brinkman <abrink@commercekitchen.com>
2016-07-03 11:27:49 -0600

    [COPY] Update static page/mailer copy

--- spec/features/public_pages_walkthrough_spec.rb
+++ spec/features/public_pages_walkthrough_spec.rb
@@ -10,7 +10,8 @@ feature "A guest user" do
   scenario "can view the about page" do
     visit root_path
     click_link "About"
-    expect(page).to have_content("About Showami")
+    expect(page).to have_content("About")
+    expect(page).to have_content("Leverage your time • Grow your business • Meet your client’s needs • Get your life back")
   end
 
   scenario "can view the privacy policy page" do
@@ -21,14 +22,16 @@ feature "A guest user" do
 
   scenario "can view the terms and conditions page" do
     visit root_path
-    click_link "Terms"
-    expect(page).to have_content("Terms and Conditions")
+    click_link "Application End User License Agreement"
+    expect(page).to have_content("Application End User License Agreement")
   end
 
   scenario "can view the contact page" do
     visit root_path
     click_link "Contact"
-    expect(page).to have_content("Contact Us")
+    expect(page).to have_content("Contact")
+    expect(page).to have_content("Have a question? Want to learn how Showami")
+    expect(page).to have_content("Contact us at admin@showami.com")
   end
 
   scenario "is not able to get to the sidekiq dashboard" do
